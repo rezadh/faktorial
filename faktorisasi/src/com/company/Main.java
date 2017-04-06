97107c6b89de2dad986f36fe59b9b474fefdfaf6
Reza Dwi Hendarno <rezadwihendarno@gmail.com>
2017-04-06 09:46:00 +0700

    Update Main.java

--- faktorisasi/src/com/company/Main.java
+++ faktorisasi/src/com/company/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         int l = 2;
-        int tingkat = 100;
-        int n = 18 / 2;
+        int tingkat = 20;
+        int n = 18 / 3;
         for (int i = 1; i <= n; i++) {
             for (int k = n; k > i; k--) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= l; j++) {
-                System.out.print("+");
+                System.out.print("#");
             }
             l += 2;
 
